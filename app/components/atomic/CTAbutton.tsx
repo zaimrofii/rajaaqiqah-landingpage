5223899e3aceac04e8c92044cf06f750a6059ebc
zaimrofii <zaimrofii20@gmail.com>
2025-11-10 10:43:22 +0700

    perubahan pada nomer wa

--- app/components/atomic/CTAbutton.tsx
+++ app/components/atomic/CTAbutton.tsx
@@ -5,7 +5,8 @@ const CTAbutton = () => {
   return (
     <div>
       <Link
-        href="https://wa.me/6281284126458"
+        href="https://wa.me/628979811467"
+        // href="https://wa.me/6281284126458"
         target="_blank"
         rel="noopener noreferrer"
         className="inline-block bg-[var(--secondary)] text-white font-semibold py-3 px-8 rounded-full shadow-lg hover:scale-105 transition-transform duration-300"
